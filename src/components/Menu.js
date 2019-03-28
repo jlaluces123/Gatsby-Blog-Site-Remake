7c8e476606010f88bded1189d8d3a8dc9edc9fa3
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-28 15:23:03 -0700

    Created Blog List Page: Used GraphQL to get the data for each blog

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,8 @@ export const Menu = () => {
     <div style = {{ backgroundColor: "#2F2F2F" }}>
       <ul style = {{ listStyle: "none", display: "flex", justifyContent: "space-evenly", width: "100%" }}>
         <li><Link to = "/">Home</Link></li>
-        <li><Link to = "/about">About</Link></li>        
+        <li><Link to = "/about">About</Link></li>    
+        <li><Link to = "/blog">Blog</Link></li>    
       </ul>
     </div>
   )
---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Blog = ({ data }) => {
+  return (
+    <div>
+      <h1>Latest Posts</h1>
+      {data.allMarkdownRemark.edges.map(post => (
+        <div key = { post.node.id }>
+          <h3>{post.node.frontmatter.title}</h3>
+          <small>Posted by: { post.node.frontmatter.author }</small>
+          <small>{ post.node.frontmatter.date }</small>
+          <Link to = {post.node.frontmatter.path}>Read More -></Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogQuery {    
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+            date
+            author
+          }            
+        }
+      }
+    }    
+  }
+`
+
+export default Blog
