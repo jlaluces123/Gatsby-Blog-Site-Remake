6f5d4eaf768410ed5834505da41e2195226d5c90
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-28 14:33:12 -0700

    Menu: Set up the menu and styled a bit, starting the blog

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export const Menu = () => {
   return (
-    <div>
-      
+    <div style = {{ backgroundColor: "#2F2F2F" }}>
+      <ul style = {{ listStyle: "none", display: "flex", justifyContent: "space-evenly", width: "100%" }}>
+        <li><Link to = "/">Home</Link></li>
+        <li><Link to = "/about">About</Link></li>        
+      </ul>
     </div>
   )
 }
