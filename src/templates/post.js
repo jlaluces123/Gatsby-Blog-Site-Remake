9c7d5f31e673465dcf9d06960e5f992d89cdbe49
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-03-28 15:30:06 -0700

    Done: Created template for individual blog pages

---
+++ src/templates/post.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, graphql } from "gatsby"
+
+/* This is the component for the individual blog pages */
+export default Template = ({ data }) => {
+  const post = data.markdownRemark
+
+  return (
+    <div>
+      <Link to = "/blog">Go Back</Link>
+      <h1>{ post.frontmatter.title }</h1>
+      <h4>by { post.frontmatter.author }</h4>
+      <small>{ post.frontmatter.date }</small>
+
+      <div dangerouslySetInnerHTML = {{ __html: post.html }} /> {/* Allows for HTML markup inside */}
+    </div>
+  )
+}
+
+/* Need to create a query for the data now */
+/* markDownRemark frontmatter where the path === path brought in */
+/* Now we need to configure a way for gatsby to recognize which page to load --> gatsby-node.js */
+export const postQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: {eq: $path} }) {
+      html
+      frontmatter {
+        path
+        title
+        author
+        date
+      }
+    }
+  }
+`
