f250b24afad2680bb93e9d51a2467e9a0af97114
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-01 15:09:38 -0700

    Base Implementation: Base styling is there, has the right idea

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/header/header"
 
 import "./blog.css"
 
 const Blog = ({ data }) => {
-  return (
-    <div>
-      <h1>Latest Posts</h1>
-      {data.allMarkdownRemark.edges.map(post => (
-        <div key = { post.node.id }>
-          <h3>{post.node.frontmatter.title}</h3>
-          <small>Posted by: { post.node.frontmatter.author }</small>
-          <small>{ post.node.frontmatter.date }</small>
-          <Link to = {post.node.frontmatter.path}>Read More -></Link>
+  return (    
+    <div className = "blog-root">
+      <Header />
+      <div className = "blog-list-wrapper">      
+        <h1>Latest Posts</h1>
+        <div className = "map-wrapper">
+
+          {data.allMarkdownRemark.edges.map(post => (
+            <div key = { post.node.id } className = "post-map-item">
+              <h3>{post.node.frontmatter.title}</h3>
+              <small>Posted by: { post.node.frontmatter.author }</small>
+              <small>{ post.node.frontmatter.date }</small>
+              <Link to = {post.node.frontmatter.path}>Read More -></Link>
+            </div>
+          ))}
+
         </div>
-      ))}
+      </div>
     </div>
   )
 }
