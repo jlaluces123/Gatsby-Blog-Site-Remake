962104b41d4c17604237a66ac25e64a1e8316901
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-01 14:50:33 -0700

    Main Page: Done for now, working on Blog list page now

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import "./index.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Welcome to the blog site</h1>     
+    <h1>The Thoughts Of A Developer</h1>     
     <div className = "image-wrapper"></div>
     <Link to="/blog/">All Posts</Link>
   </Layout>
