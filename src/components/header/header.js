f7057c28ce19941452dc87b2aa1c3c20f2501c8b
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-04-02 09:00:57 -0700

    Header styling + Page Layout: Fixed header so it spans full width, need to add max-width on it as well as finish up styling the blog page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,8 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Menu } from "../menu/Menu.js"
 
+import "./header.css"
+
 
 const Header = ({ siteTitle }) => (
   <header className = "menu-nav-wrapper">
