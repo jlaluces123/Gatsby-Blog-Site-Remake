24d6a864fb9fed8c4ca57f40d5a595af744d6f06
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-08-08 16:15:41 -0700

    About Page Tweaks: got rid of the image and fixed the mobile view of the app

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Header from "../components/header/header.js"
-import Img from "gatsby-image"
 import { graphql } from "gatsby"
 
 import "./about.css"
@@ -10,22 +9,19 @@ const About = (props) => {
     <div className = "about-page-wrapper">
       <Header />
       <h1 className = "page-title">About Me</h1>
-      <div className = "content-wrapper">          
-        <div className = "large-img-wrapper left-side"> 
-          <Img className = "profile-img" fluid={props.data.imageOne.childImageSharp.fluid} />          
-        </div>
+      <div className = "content-wrapper">
 
-        <div className = "large-text-wrapper right-side">
-          <section className = "summary-wrapper">   
-            <h3 className = "section-header">So Who Am I?</h3>             
+        <div className = "large-text-wrapper">
+          <section className = "summary-wrapper">
+            <h3 className = "section-header">So Who Am I?</h3>
             <p className = "summary-paragraph">
-              My name is Jonathan Laluces. For over 10 years I have been a classical musician playing the cello. It was my life long dream to become a successful musician! I auditioned and was 
-              admitted to numerous music programs in and out of school. Shortly after graduating High School in June of 2018, I began my newest journey, becoming a web developer. 
+              My name is Jonathan Laluces. For over 10 years I have been a classical musician playing the cello. It was my life long dream to become a successful musician! I auditioned and was
+              admitted to numerous music programs in and out of school. Shortly after graduating High School in June of 2018, I began my newest journey, becoming a web developer.
 
               <br />
               <br />
-              The thing I love most about the world of web development is the challenge. Technologies, languages, and stacks are all changing constantly and it forces me to keep on my toes. Web 
-              development provides a certain type of difficulty that I can't experience at any other type of job.       
+              The thing I love most about the world of web development is the challenge. Technologies, languages, and stacks are all changing constantly and it forces me to keep on my toes. Web
+              development provides a certain type of difficulty that I can't experience at any other type of job.
             </p>
           </section>
 
@@ -39,12 +35,12 @@ const About = (props) => {
               <li className = "facts-item">Aside from coding and music, I am an avid runner, always attending the annual 10K run in my area</li>
             </ul>
           </section>
-         
+
         </div>
 
       </div>
     </div>
-  ) 
+  )
 }
 
 export const pageQuery = graphql`
